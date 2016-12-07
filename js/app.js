84653e39f3b314380a4923b930da4f3bd65fa18b
aijazkhan81 <aijazkhan81@gmail.com>
2016-12-07 02:06:08 -0600

    cosmetic changes and refactoring remaining

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
-$('.content').slideUp();
+$('.content').hide();
+$( ".heading" ).append( '<span class="arrow"></span>' );
+
 
 $('.heading').click(function(){
-	$(this).next().slideToggle(500);
+	var at = $(this).attr('data-heading');
+	var con = $("[data-content='" + at + "']");
+	con.slideToggle(200);
+	if(con.attr('newTile')){
+		con.removeAttr('newTile');
+		var degree = 0;
+		$(this).find( ".arrow" ).css({
+			'-webkit-transform': 'rotate(' + degree + 'deg)',
+			'-moz-transform': 'rotate(' + degree + 'deg)',
+			'-ms-transform': 'rotate(' + degree + 'deg)',
+			'-o-transform': 'rotate(' + degree + 'deg)',
+			'transform': 'rotate(' + degree + 'deg)',
+			'zoom': 1
+		});
+	}
+	else{
+		con.attr('newTile', at);
+		var degree = -90;
+		$(this).find( ".arrow" ).css({
+			'-webkit-transform': 'rotate(' + degree + 'deg)',
+			'-moz-transform': 'rotate(' + degree + 'deg)',
+			'-ms-transform': 'rotate(' + degree + 'deg)',
+			'-o-transform': 'rotate(' + degree + 'deg)',
+			'transform': 'rotate(' + degree + 'deg)',
+			'zoom': 1
+		});
+	}
+	
 })
+
