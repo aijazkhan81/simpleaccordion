8e1513bac369545adce8df5b78023599e6c27be3
aijazkhan81 <aijazkhan81@gmail.com>
2016-12-08 00:55:16 -0600

    removed show more and moved into new repo

--- js/app.js
+++ js/app.js
@@ -35,14 +35,3 @@ $('.heading').click(function(){
 
 // Code end for accordion
 
-// Begin code for Show More 
-
-$('.flex-item').append('<button class="showmore"><span class="plus">+</span>Show More</button>');
-
-$('.showmore').click(function(){
-	$(this).prev().toggleClass('fulltext');
-	var html = $(this).html();
-	$(this).html(html == '<span class="plus">+</span>Show More' ? '<span class="plus">-</span>Show Less' : '<span class="plus">+</span>Show More' );
-});
-
-// Code end for Show More
